1e8a142ffb4239ce97dd869acca8c7a97be5f99c
Akanksha Jain <akanksha.jain@ibm.com>
2021-01-15 09:47:27 -0500

    Adding loading image

--- static/js/operators.js
+++ static/js/operators.js
@@ -1,12 +1,12 @@
 function createHelmoperator(){
     console.log("in js")
+    $('#loadingmessage').show();
     var chart_name = $("#chart_name").val();
     var chart_repo = $("#chart_repo").val();
     var op_name = $("#op_name").val();
     var requestBody = "{\"helmchartname\" : \"" + chart_name +
                       "\",\"helmrepo\" : \"" + chart_repo +
                       "\" ,\"operatorname\" : \"" + op_name +"\"}";
-    console.log(requestBody)
 	$.ajax({
 		type : 'POST',
 		url :  'http://9.30.199.16:5000/helmoperator',
@@ -16,11 +16,16 @@ function createHelmoperator(){
 		},
 		dataType : "json",
 		success : function(data) {
+		    $('#loadingmessage').hide();
 		    console.log("yeyyyy")
 	        renderSuccessMessage(" Operator added successfully at /root/operators/" + op_name);
 		},
 		error: function(jqXHR, textStatus, errorThrown) {
+		    $('#loadingmessage').hide();
 		    console.log("nooo")
+		    console.log(textStatus)
+		    console.log(jqXHR)
+		    console.log(errorThrown)
 		    renderSuccessMessage(" Operator added successfully at /root/operators/" + op_name);
 		}
 	});

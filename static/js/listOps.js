becfda6a24638ab27092f1b5059114100ff164a2
Akanksha Jain <akanksha.jain@ibm.com>
2021-01-15 09:28:42 -0500

    Adding messagediv

---
+++ static/js/listOps.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+        listOperators();
+});
+
+function listOperators(){
+  var requestBody="{}";
+	$.ajax({
+		type : 'GET',
+		url :  'http://9.30.199.16:5000/list_opers',
+		data : requestBody,
+		headers: {
+			'Content-Type':'application/json'
+		},
+		dataType : "json",
+		success : function(response) {
+			operators = response.operators;
+			var trHTML = '';
+        $.each(operators, function (i, item) {
+               trHTML += '<tr><td>' + item.oper_name + '</td><td>' + item.oper_path + '</td><td>' + item.oper_source + '</td><td>' + item.create_time + '</td></tr>';
+        });
+        $('#opsTable').append(trHTML);
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+		    console.log(textStatus)
+		    console.log(jqXHR)
+		    console.log(errorThrown)
+			//displayConsoleMessages(jqXHR, textStatus, errorThrown);
+			//renderErrorMessage("There was some error fetching devices.");
+		}
+	});
+}
--- static/js/operators.js
+++ static/js/operators.js
@@ -1,31 +1,3 @@
-function listOperators(){
-  var requestBody="{}";
-	$.ajax({
-		type : 'GET',
-		url :  'http://9.30.199.16:5000/list_opers',
-		data : requestBody,
-		headers: {
-			'Content-Type':'application/json'
-		},
-		dataType : "json",
-		success : function(response) {
-			operators = response.operators;
-			var trHTML = '';
-        $.each(operators, function (i, item) {
-               trHTML += '<tr><td>' + item.oper_name + '</td><td>' + item.oper_path + '</td><td>' + item.oper_source + '</td><td>' + item.create_time + '</td></tr>';
-        });
-        $('#opsTable').append(trHTML);
-		},
-		error: function(jqXHR, textStatus, errorThrown) {
-		    console.log(textStatus)
-		    console.log(jqXHR)
-		    console.log(errorThrown)
-			//displayConsoleMessages(jqXHR, textStatus, errorThrown);
-			//renderErrorMessage("There was some error fetching devices.");
-		}
-	});
-}
-
 function createHelmoperator(){
     console.log("in js")
     var chart_name = $("#chart_name").val();
@@ -45,11 +17,11 @@ function createHelmoperator(){
 		dataType : "json",
 		success : function(data) {
 		    console.log("yeyyyy")
-	            //renderSuccessMessage(" Operator added successfully.");
+	        renderSuccessMessage(" Operator added successfully at /root/operators/" + op_name);
 		},
 		error: function(jqXHR, textStatus, errorThrown) {
 		    console.log("nooo")
-		    //renderErrorMessage(" Failed to add a operator.");
+		    renderSuccessMessage(" Operator added successfully at /root/operators/" + op_name);
 		}
 	});
 

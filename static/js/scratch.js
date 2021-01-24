6d4bd10122e7512c0a9245426dd4631929989dc8
akankshajain <akanksha_jain@persistent.co.in>
2021-01-24 12:57:58 -0500

    Fixing port type and some modals

--- static/js/scratch.js
+++ static/js/scratch.js
@@ -40,11 +40,10 @@ $(document).ready(function() {
 
     $('#full_createkind').click(function   (event) {
         var kindVal = $('#full_kind').val();
-        var kind = {
-            name: kindVal,
-            "resourcenames": resourcenames
-        }
+        var kind = "{\"name\":\"" +  kindVal + "\"," +
+            "\"resourcenames\":[" + resourcenames + "]}"
         kinds.push(kind)
+        resourcenames = []
         $('#full_bar').append('&nbsp;&nbsp;<button class="btn-styled" type="button">' + kindVal + '</button>');
 
     });
@@ -59,7 +58,7 @@ $(document).ready(function() {
                           "\",\"domainname\" : \"" + domainName +
                           "\",\"operatorname\" : \"" + operatorName +
                           "\",\"version\" : \"" + version +
-                          "\" ,\"kinds\" :" +  JSON.stringify(kinds) + "}";
+                          "\" ,\"kinds\" : [" +  kinds + "]}";
         console.log(requestBody)
 
     });
@@ -72,40 +71,37 @@ $(document).ready(function() {
 
     // process add resource button and create
     $('#dep_add').click(function   (event) {
-        Deployment = {
+        Deployment = JSON.stringify({
             type: "Deployment",
             name : $('#dname').val(),
-            port : $('#port').val(),
+            port : parseFloat($('#port').val()),
             image : $('#image').val(),
             label : $('#label').val(),
             replicas: $('#replica').val()
 
-        }
-        resourcenames.push(Deployment);
-        $('.modal').on('hidden.bs.modal', function(){
-            $(this).removeData('bs.modal');
         });
+        resourcenames.push(Deployment);
     });
 
     $('#service_add').click(function   (event) {
-        Service = {
+        Service = JSON.stringify({
             type: "Service",
             name : $('#sname').val(),
-            sourceport : $('#sport').val(),
-            targetport : $('#tport').val(),
+            sourceport : parseFloat($('#sport').val()),
+            targetport : parseFloat($('#tport').val()),
             podselectorlabel : $('#label').val()
 
-        }
+        });
         resourcenames.push(Service);
     });
 
     $('#route_add').click(function   (event) {
-        Route = {
+        Route = JSON.stringify({
             type: "Route",
             name : $('#rname').val(),
             servicename : $('#servicename').val(),
-            targetport : $('#targetport').val()
-        }
+            targetport : parseFloat($('#targetport').val())
+        });
         resourcenames.push(Route);
     });
 
